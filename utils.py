9c0dd5f903f5c3ac59a2e215ef98ca082f04bdc2
Joyjeet Roy <joyjeetroy3@gmail.com>
2025-07-19 01:16:02 +0530

    Added Docker Compose

--- utils.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime, timedelta
-
-def print_current_datetime():
-    now = datetime.now()+timedelta(hours=5)
-    
-    print(f"[Utils] Current datetime: {now}")
-
-print_current_datetime()
